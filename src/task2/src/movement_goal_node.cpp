fdcc4cf81582616d2d96e42f57359c1ee3dc45e8
Rozles <rozlesterle@gmail.com>
2023-05-04 02:28:34 +0200

    progress

--- src/task2/src/movement_goal_node.cpp
+++ src/task2/src/movement_goal_node.cpp
@@ -31,10 +31,13 @@
 tf2_ros::Buffer tf2_buffer;
 ros::Publisher pubx;
 
+ros::Publisher pub;
+
 int CENTER_X = 320;
 int CENTER_Y = 240;
 
 typedef pcl::PointXYZRGB PointT;
+
 
 pcl::PointCloud<PointT>::Ptr cloud(new pcl::PointCloud<PointT>);
 pcl::PointCloud<pcl::Normal>::Ptr cloud_normals(new pcl::PointCloud<pcl::Normal>);
@@ -55,98 +58,82 @@ void mark_point(int x, int y)
 }
 
 bool serviceCallBack(task2::NewGoalService::Request& req, task2::NewGoalService::Response& res) {
-    if (cloud->size() == 0) return false;
-
-    std::cout << cloud->header.frame_id;
+    if (cloud->size() == 0) return false;    
     
-    pcl::Normal n_cloud = cloud_normals->at(320, 240);
-    PointT p_cloud = cloud->at(320, 240);
-    geometry_msgs::PointStamped p1;
-    geometry_msgs::PointStamped normal_cloud;
-    for (int i = CENTER_X - 10; i < CENTER_X + 10; i++) {
-        for (int j = CENTER_Y - 10; j < CENTER_Y + 10; j++) {
-            pcl::Normal n_cloud = cloud_normals->at(i, j);
-            normal_cloud.point.x += n_cloud.normal_x;
-            normal_cloud.point.y += n_cloud.normal_y;
-            normal_cloud.point.z += n_cloud.normal_z;
-        }
-    }
-    normal_cloud.point.x = normal_cloud.point.x /100;
-    normal_cloud.point.y = normal_cloud.point.y /100;
-    normal_cloud.point.z = normal_cloud.point.z /100;
+    pcl::Normal n = cloud_normals->at(320, 240);
+    PointT p = cloud->at(320, 240);
 
-    geometry_msgs::PointStamped p;
-    geometry_msgs::PointStamped n;
-    p1.point.x = p_cloud.x;
-    p1.point.y = p_cloud.y;
-    p1.point.z = p_cloud.z;
-    p1.header.stamp = cloud_stamp;
-    p1.header.frame_id = cloud->header.frame_id;
+
+
+    geometry_msgs::PointStamped t;
+    geometry_msgs::PointStamped goal;
+
+    float dot = n.normal_x * p.x + n.normal_y * p.y + n.normal_z * p.z;
+  
+    float vx = (1.0 - 2 * dot * n.normal_x);
+    float vy = (0.0 - 2 * dot * n.normal_y);
+    float vz = (0.0 - 2 * dot * n.normal_z);
+    t.point.x = p.x + n.normal_x;
+    t.point.y = p.y + n.normal_y;
+    t.point.z = p.z + n.normal_z;
+    t.header.stamp = cloud_stamp;
+    t.header.frame_id = cloud->header.frame_id;
+    
     try {
-        p = tf2_buffer.transform(p1, "map");
+        goal = tf2_buffer.transform(t, "map");
     } catch (tf2::TransformException &e) {
         ROS_WARN("Transform warning: %s\n", e.what());
         return false;
     }
 
-    normal_cloud.header.stamp = cloud_stamp;
-    normal_cloud.header.frame_id = cloud->header.frame_id; 
-    try { 
-        n = tf2_buffer.transform(normal_cloud, "map");
-    }
-    catch (tf2::TransformException &e) {
-        ROS_WARN("Transform warning: %s\n", e.what());
-        return false;
-    }
+    visualization_msgs::Marker marker;
 
-    geometry_msgs::PointStamped robot;
-    robot.point.x = 0;
-    robot.point.y = 0;
-    robot.point.z = 0;
-    robot.header.stamp = cloud_stamp;
-    robot.header.frame_id = "base_link";
-    try { 
-        robot = tf2_buffer.transform(robot, "map");
-    }
-    catch (tf2::TransformException &e) {
-        ROS_WARN("Transform warning: %s\n", e.what());
-        return false;
-    }
+    marker.header.frame_id = "map";
+    marker.header.stamp = ros::Time::now();
 
-    ROS_INFO("Robot X: %f, Y: %f, Z: %f", robot.point.x, robot.point.y, robot.point.z);
+    marker.ns = "cylinder";
+    marker.id = 0;
 
-    
-    float len = std::sqrt(n.point.x * n.point.x + n.point.y * n.point.y + n.point.z * n.point.z); 
-    n.point.x = n.point.x / len;
-    n.point.y = n.point.y / len;
-    n.point.z = n.point.z / len;
-    
-    
+    marker.type = visualization_msgs::Marker::CYLINDER;
+    marker.action = visualization_msgs::Marker::ADD;
 
+    marker.pose.position.x = t.point.x;
+    marker.pose.position.y = t.point.y;
+    marker.pose.position.z = t.point.z;
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;
 
-    ROS_INFO("Point X: %f, Y: %f, Z: %f", p.point.x, p.point.y, p.point.z);
-    ROS_INFO("Normal X: %f, Y: %f, Z: %f", n.point.x, n.point.y, n.point.z);
+    marker.scale.x = 0.1;
+    marker.scale.y = 0.1;
+    marker.scale.z = 0.1;
 
-    float vec_x = -(p.point.x - robot.point.x);
-    float vec_y = -(p.point.x - robot.point.x);
+    marker.color.r=0.0f;
+    marker.color.g=1.0f;
+    marker.color.b=0.0f;
+    marker.color.a=1.0f;
 
-    float angle = std::acos((n.point.x * vec_x + n.point.y * vec_y) / (std::sqrt(vec_x * vec_x + vec_y * vec_y) * std::sqrt(n.point.x * n.point.x + n.point.y * n.point.y)));
-    
-    ROS_INFO("Angle: %f", angle);
-    geometry_msgs::Quaternion quaternion;
-    tf2::Quaternion q;
-    q.setRPY(0.0, 0.0, -angle);
-    tf2::convert(q, quaternion);
-    
-    geometry_msgs::PoseStamped goal_pose;
-    goal_pose.pose.position.x = p.point.x + n.point.x * 0.5;
-    goal_pose.pose.position.y = p.point.y + n.point.y * 0.5;
-    goal_pose.pose.orientation = quaternion;
-    goal_pose.header.stamp = req.time;
-    goal_pose.header.frame_id = "map";
+    marker.lifetime = ros::Duration();
+
+    pub.publish (marker);
+
+    ROS_INFO("GOAL X %f, Y %f, Z: %f", t.point.x, t.point.y, t.point.z);
+
+    // geometry_msgs::Quaternion quaternion;
+    // tf2::Quaternion q;
+    // q.setRPY(0.0, 0.0, rangle);
+    // tf2::convert(q, quaternion);
+
+    // geometry_msgs::PoseStamped goal_pose;
+    // goal_pose.pose.position.x = p.point.x + n.point.x * 0.5;
+    // goal_pose.pose.position.y = p.point.y + n.point.y * 0.5;
+    // goal_pose.pose.orientation = quaternion;
+    // goal_pose.header.stamp = req.time;
+    // goal_pose.header.frame_id = "map";
 
 
-    res.goal = goal_pose;
+    // res.goal = goal_pose;
     return true;
    
 }
@@ -158,14 +145,14 @@ void cloud_cb(const pcl::PCLPointCloud2ConstPtr &cloud_blob) {
 
     ne.setNormalEstimationMethod(ne.AVERAGE_3D_GRADIENT);
     ne.setMaxDepthChangeFactor(0.02f);
-    ne.setNormalSmoothingSize(10.0f);
+    ne.setNormalSmoothingSize(100.0f);
     ne.setInputCloud(cloud);
     ne.compute(*cloud_normals);
 
-    //     mark_point(320, 240);
-    //     pcl::PCLPointCloud2 outcloud;
-    //     pcl::toPCLPointCloud2(*cloud, outcloud);
-    //     pubx.publish(outcloud);
+    // mark_point(320, 240);
+    // pcl::PCLPointCloud2 outcloud;
+    // pcl::toPCLPointCloud2(*cloud, outcloud);
+    // pubx.publish(outcloud);
 }
 
 int main(int argc, char **argv)
@@ -181,10 +168,11 @@ int main(int argc, char **argv)
     tf2_ros::TransformListener tf2_listener(tf2_buffer);
 
     ros::Subscriber sub = nh.subscribe("/camera/depth/points", 1, cloud_cb);
+    pub = nh.advertise<visualization_msgs::Marker>("test", 1);
 
     ros::ServiceServer service = nh.advertiseService("new_goal_service", serviceCallBack);
 
-    pubx = nh.advertise<pcl::PCLPointCloud2>("face_normal_cloud", 1);
+    //pubx = nh.advertise<pcl::PCLPointCloud2>("normals_cloud", 1);
 
     ros::spin();
 
--- src/task2/src/mozgancki.cpp
+++ src/task2/src/mozgancki.cpp
@@ -10,38 +10,55 @@
 #include "geometry_msgs/PoseWithCovarianceStamped.h"
 #include "geometry_msgs/Quaternion.h"
 #include "geometry_msgs/Vector3.h"
-#include "sensor_msgs/Image.h"
 #include "tf2/LinearMath/Quaternion.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 #include <vector>
 
-#include "task2/NewGoalService.h"
-
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 std::vector<geometry_msgs::PoseStamped> goals;
-std::vector<int> face_goal_indices;
-std::vector<bool> known_faces;
+
+ros::Publisher park;
 
 MoveBaseClient *acPtr;
-ros::ServiceClient newGoalClient;
 
 float robot_x;
 float robot_y;
 
+float green_x;
+float green_y;
+
+int cylinders = 0;
+int rings = 0;
+
+bool green_found = false;
+ros::Time time = ros::Time(0);
+
+const int GOAL_NUMBER = 17;
+
 int NUMBER_OF_FACES = 3;
 
 
-void generateNewGoal() {
-    task2::NewGoalService srv;
-    srv.request.time = ros::Time::now();
-
-    if (newGoalClient.call(srv)){
-        goals.insert(goals.begin(), srv.response.goal);
-    } else {
-        ROS_ERROR("New goal service failed.");
-    }
-}
+// goal coordinates x, y, q:[z, w]
+float goal_cords[GOAL_NUMBER][4] = {
+  {-0.72,  -0.31, 0.97, 0.23},
+  {-0.63,  -0.35, 0.96, -0.27},
+  {-0.15,  -0.56, -0.34, 0.94}, 
+  {0.32,  -1.67, 0.99, 0.16},
+  {1.42,  -1.6, -0.36, 0.93},
+  {3.37,  -1.0, -0.74, 0.67},
+  {2.55,  -0.89, 0.44, 0.90},
+  {1.50,  0.27, 0.98, 0.10},
+  {1.18,  -0.32, -0.55, 0.83},
+  {2.44,  1.13, 0.40, 0.92},
+  {2.44,  2.16, -0.92, 0.40},
+  {1.58,  2.11, 0.96, 0.27},
+  {0.54,  2.05, -0.86, 0.52},
+  {0.38,  1.94, 0.92, 0.40},
+  {-1.36,  1.79, 0.44, 0.90},
+  {-0.67,  1.39, -0.92, 0.40},
+  {0.0, 0.0, 0.70, 0.70}
+};
 
 
 void positionCallBack(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg) {
@@ -49,40 +66,47 @@ void positionCallBack(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& 
     robot_y = msg->pose.pose.position.y;
 }
 
-void markerCallBack(const visualization_msgs::MarkerArray::ConstPtr& markerArray){
-    visualization_msgs::Marker marker;
-    for (int i = 0; i < markerArray->markers.size(); i++) {
-        if (i >= known_faces.size()) 
-            known_faces.push_back(false);
-        if (!known_faces[i]) {
-            marker = markerArray->markers[i];
-            tf2::Vector3 vector(0.5, 0, 0);
-            tf2::Vector3 rotated = tf2::quatRotate(tf2::Quaternion(marker.pose.orientation.x, marker.pose.orientation.y, marker.pose.orientation.z, marker.pose.orientation.w), vector);
-            geometry_msgs::PoseStamped goal;
-            goal.header.frame_id = "map";
-            goal.pose.position.x = marker.pose.position.x + rotated[0];
-            goal.pose.position.y = marker.pose.position.y + rotated[1];
-            tf2::Vector3 turn_to_marker(marker.pose.position.x - goal.pose.position.x, marker.pose.position.y - goal.pose.position.y, 0.0);
-            tf2::Quaternion turn_to_marker_q;
-            turn_to_marker_q.setRPY(0, 0, atan2(turn_to_marker.y(), turn_to_marker.x()));
-            goal.pose.orientation = tf2::toMsg(turn_to_marker_q);
-            // if (face_goal_indices[0] == i) {
-            //     goals.erase(goals.begin());
-            //     face_goal_indices.erase(face_goal_indices.begin());
-            // }
-            goals.erase(goals.begin());
-            face_goal_indices.erase(face_goal_indices.begin());
-            goals.insert(goals.begin(), goal);
-            face_goal_indices.insert(face_goal_indices.begin(), i);
-            acPtr->cancelGoal(); 
-    
-            break;
-        }   
-    }    
+void ringCallBack(const visualization_msgs::MarkerArray::ConstPtr& markerArray) {
+    rings = markerArray->markers.size();
+    for(visualization_msgs::Marker m : markerArray->markers) {
+        if (m.color.g > 200) {
+            if (time == ros::Time(0)) 
+                time = ros::Time::now();
+            green_x = m.pose.position.x;
+            green_y = m.pose.position.y;
+        }
+    }
+}
+
+void setGreenGoal() {
+    geometry_msgs::PoseStamped goal;
+    float vx = green_x - robot_x;
+    float vy = green_y - robot_y;
+    float dist = sqrt(vx * vx + vy * vy);
+    vx = vx / dist * (dist - 0.5);
+    vy = vy / dist * (dist - 0.5); 
+    goal.pose.position.x = robot_x + vx;
+    goal.pose.position.y = robot_y + vy;
+    goal.header.frame_id = "map";
+    tf2::Vector3 turn_to_marker(green_x - goal.pose.position.x, green_y - goal.pose.position.y, 0.0);
+    tf2::Quaternion turn_to_marker_q;
+    turn_to_marker_q.setRPY(0, 0, atan2(turn_to_marker.y(), turn_to_marker.x()));
+    goal.pose.orientation = tf2::toMsg(turn_to_marker_q);
+    goals.insert(goals.begin(), goal);
+}
+
+void cylinderCallBack(const visualization_msgs::MarkerArray::ConstPtr& markerArray) {
+    cylinders = markerArray->markers.size();
 }
 
 void moveToGoals() {
-    if (goals.empty()) generateNewGoal();
+    if (goals.empty()) return;  
+    bool green_goal = false;
+    ros::Duration time_passed = ros::Time::now() - time;
+    if (rings == 4 && cylinders == 4 || time_passed > ros::Duration(120.0)) {
+        green_goal = true;
+        setGreenGoal();
+    }
 
     geometry_msgs::PoseStamped goal = goals[0];
     move_base_msgs::MoveBaseGoal target;
@@ -91,11 +115,19 @@ void moveToGoals() {
     acPtr->waitForResult();
     
     if (acPtr->getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
-        generateNewGoal();
+        // ROS_INFO("Reached goal: %f : %f", goal.pose.position.x, goal.pose.position.y);
+        if (green_goal) {
+            park.publish("start");
+            ros::shutdown();
+        }
+        goals.erase(goals.begin());
+        moveToGoals();
     } else  if (acPtr->getState() == actionlib::SimpleClientGoalState::RECALLED || acPtr->getState() == actionlib::SimpleClientGoalState::PREEMPTED){
         moveToGoals();
     } else {
-        ROS_WARN("Goal could not ne reached");    
+        ROS_WARN("Goal could not ne reached");
+        goals.erase(goals.begin());
+        moveToGoals();
     }
 }
 
@@ -107,17 +139,26 @@ void actionClientThread(MoveBaseClient *actionClient) {
 }
 
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "mozgancki");
+    ros::init(argc, argv, "solution");
     ros::NodeHandle n;
 
     MoveBaseClient actionClient("/move_base", true);
     acPtr = &actionClient;
+    ros::Subscriber sub_ring = n.subscribe("ring_markers", 100, ringCallBack);
+    ros::Subscriber sub_cylinder = n.subscribe("culinder_markers", 1, cylinderCallBack);
+    park = n.advertise<std_msgs::String>("start_parking", 1);
 
-    ros::Subscriber sub = n.subscribe("face_markers", 100, markerCallBack);
+    for (int i = 0; i < GOAL_NUMBER; i++) {
+        geometry_msgs::PoseStamped goal;
+        goal.header.frame_id = "map";
+        goal.pose.position.x = goal_cords[i][0];
+        goal.pose.position.y = goal_cords[i][1];
+        goal.pose.orientation.z = goal_cords[i][2];
+        goal.pose.orientation.w = goal_cords[i][3];
+        goals.push_back(goal);
+    }
 
     ros::Subscriber position = n.subscribe("amcl_pose", 1, positionCallBack);
-
-    newGoalClient = n.serviceClient<task2::NewGoalService>("new_goal_service");
 
     boost::thread thread(boost::bind(&actionClientThread, &actionClient));
 
