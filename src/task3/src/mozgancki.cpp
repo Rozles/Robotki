0f3283ecd02b1a461a5d4ce3ed35868a63344feb
Rozles <rozlesterle@gmail.com>
2023-05-13 20:39:37 +0200

    movement 1 normale iz point clouda

--- src/task3/src/mozgancki.cpp
+++ src/task3/src/mozgancki.cpp
@@ -14,11 +14,14 @@
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 #include <vector>
 
+#include "task3/NewGoalService.h"
+
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 std::vector<geometry_msgs::PoseStamped> goals;
 
 ros::Publisher park;
+ros::ServiceClient client;
 
 MoveBaseClient *acPtr;
 
@@ -35,31 +38,6 @@ int rings = 0;
 
 bool green_found = false;
 ros::Time time_green = ros::Time(0);
-
-const int GOAL_NUMBER = 16;
-
-int NUMBER_OF_FACES = 3;
-
-
-// goal coordinates x, y, q:[z, w]
-float goal_cords[GOAL_NUMBER][4] = {
-  {-0.70,  1.3, 0.87, 0.50},
-  {-0.84,  -0.36, 0.99, 0.16}, 
-  {0.0,  -1.70, 0.09, 0.99},
-  {1.42,  -1.6, -0.36, 0.93},
-  {3.37,  -1.0, -0.74, 0.67},
-  {2.55,  0.57, -0.93, 0.37},
-  {1.70,  0.27, 0.98, 0.10},
-  {1.18,  -0.32, -0.55, 0.83},
-  {2.44,  1.13, 0.40, 0.92},
-  {2.44,  2.16, -0.92, 0.40},
-  {1.58,  2.11, 0.96, 0.27},
-  {0.54,  2.05, -0.86, 0.52},
-  {0.38,  1.94, 0.92, 0.40},
-  {-1.36,  1.79, 0.44, 0.90},
-  {-0.67,  1.39, -0.92, 0.40},
-  {0.0, 0.0, 0.70, 0.70}
-};
 
 
 void positionCallBack(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg) {
@@ -108,15 +86,16 @@ void cylinderCallBack(const visualization_msgs::MarkerArray::ConstPtr& markerArr
 }
 
 void moveToGoals() {
-    if (goals.empty()) return;  
-    bool green_goal = false;
-    if (green_found) {
-        ros::Duration time_passed = ros::Time::now() - time_green;
-        if (rings == 4 && cylinders == 4 || time_passed > ros::Duration(120.0)) {
-            green_goal = true;
-            setGreenGoal(false);
+    if (goals.empty()) {
+        task3::NewGoalService srv;
+        srv.request.time = ros::Time::now();
+        if (client.call(srv)) {
+            task3::NewGoalService::Response response = srv.response;
+        } else {
+            ROS_ERROR("Failed to call service new_goal_service");
         }
-    }
+        return;
+    }  
     
     geometry_msgs::PoseStamped goal = goals[0];
     move_base_msgs::MoveBaseGoal target;
@@ -126,22 +105,13 @@ void moveToGoals() {
     
     if (acPtr->getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
         // ROS_INFO("Reached goal: %f : %f", goal.pose.position.x, goal.pose.position.y);
-        ROS_INFO("Rings found: %d | Cylinders found: %d, goal: %d", rings, cylinders, goali);
-        goali++;
-        if (green_goal) {
-            std_msgs::String msg;
-            msg.data = "start";
-            park.publish(msg);
-            ros::shutdown();
-        }
         goals.erase(goals.begin());
         moveToGoals();
     } else  if (acPtr->getState() == actionlib::SimpleClientGoalState::RECALLED || acPtr->getState() == actionlib::SimpleClientGoalState::PREEMPTED){
         moveToGoals();
     } else {
         ROS_WARN("Goal could not ne reached");
-        if (green_goal) setGreenGoal(true);
-        else goals.erase(goals.begin());
+        //goals.erase(goals.begin());
         moveToGoals();
     }
 }
@@ -163,15 +133,8 @@ int main(int argc, char **argv) {
     ros::Subscriber sub_cylinder = n.subscribe("cylinder_markers", 1, cylinderCallBack);
     park = n.advertise<std_msgs::String>("start_parking", 1);
 
-    for (int i = 0; i < GOAL_NUMBER; i++) {
-        geometry_msgs::PoseStamped goal;
-        goal.header.frame_id = "map";
-        goal.pose.position.x = goal_cords[i][0];
-        goal.pose.position.y = goal_cords[i][1];
-        goal.pose.orientation.z = goal_cords[i][2];
-        goal.pose.orientation.w = goal_cords[i][3];
-        goals.push_back(goal);
-    }
+    client = n.serviceClient<task3::NewGoalService>("new_goal_service");
+
 
     ros::Subscriber position = n.subscribe("amcl_pose", 1, positionCallBack);
 
